703447143fb868f1abb218d3a05c542a9f6251e4
Sewen Thy <sewen.thy@ahrefs.com>
2024-03-08 11:20:35 +0800

    ocamlformat

--- lib/api.ml
+++ lib/api.ml
@@ -12,17 +12,29 @@ module type S = sig
 
   (* conversations *)
   val get_replies :
-    ctx:Context.t -> conversation:conversations_replies_req -> conversations_replies_res slack_response Lwt.t
+     ctx:Context.t ->
+    conversation:conversations_replies_req ->
+    conversations_replies_res slack_response Lwt.t
+
   val get_conversations_info :
-    ctx:Context.t -> conversation:conversations_info_req -> conversations_info_res slack_response Lwt.t
+     ctx:Context.t ->
+    conversation:conversations_info_req ->
+    conversations_info_res slack_response Lwt.t
+
   val join_conversation : ctx:Context.t -> channel:conversations_join_req -> conversations_join_res slack_response Lwt.t
 
   (* usergroups *)
   val update_usergroup_users :
-    ctx:Context.t -> usergroup:update_usergroups_users_req -> update_usergroups_users_res slack_response Lwt.t
+     ctx:Context.t ->
+    usergroup:update_usergroups_users_req ->
+    update_usergroups_users_res slack_response Lwt.t
+
   val list_usergroups : ctx:Context.t -> req:list_usergroups_req -> list_usergroups_res slack_response Lwt.t
+
   val list_usergroup_users :
-    ctx:Context.t -> usergroup:list_usergroup_users_req -> list_usergroup_users_res slack_response Lwt.t
+     ctx:Context.t ->
+    usergroup:list_usergroup_users_req ->
+    list_usergroup_users_res slack_response Lwt.t
 
   (* users *)
   val get_user : ctx:Context.t -> user:user_info_req -> user_info_res slack_response Lwt.t
--- lib/mrkdwn.ml
+++ lib/mrkdwn.ml
@@ -7,9 +7,11 @@ let escape_mrkdwn =
     | '<' -> "&lt;"
     | '>' -> "&gt;"
     | '&' -> "&amp;"
-    | c -> String.make 1 c)
+    | c -> String.make 1 c
+    )
 
 let escaped_mrkdwn_re = Re2.create_exn {|(?P<escaped>&lt;|&gt;|&amp;)|}
+
 let unescape_mrkdwn str =
   try
     Re2.replace_exn escaped_mrkdwn_re str ~f:(fun m ->
@@ -17,7 +19,8 @@ let unescape_mrkdwn str =
       | "&lt;" -> "<"
       | "&gt;" -> ">"
       | "&amp;" -> "&"
-      | e -> Common.slack_lib_fail "impossible re2 mrkdwn unescape match: %s" e)
+      | e -> Common.slack_lib_fail "impossible re2 mrkdwn unescape match: %s" e
+    )
   with Re2.Exceptions.Regex_match_failed _ -> str
 
 let link ~url ?text () =
@@ -26,19 +29,12 @@ let link ~url ?text () =
   | None -> sprintf "<%s>" url
 
 let bold = sprintf "*%s*"
-
 let italicize = sprintf "_%s_"
-
 let strike = sprintf "~%s~"
-
 let line_break = sprintf "%s\n"
-
 let blk_quote = sprintf ">%s"
-
 let inline_code = sprintf "`%s`"
-
 let multiline_code = sprintf "```%s```"
-
 let list l = sprintf "- %s" (String.concat "\n- " l)
 
 (* https://api.slack.com/reference/surfaces/formatting#mentioning-users *)
