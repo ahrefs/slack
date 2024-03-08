e28f46defc7d90ee80c33a99af6f6d43ce3ea0ba
Sewen Thy <sewen.thy@ahrefs.com>
2024-03-08 11:24:26 +0800

    work better with mentions in mrkdwn

--- lib/mrkdwn.ml
+++ lib/mrkdwn.ml
@@ -42,3 +42,16 @@ let mention_user member_id = sprintf "<@%s>" member_id
 
 (* https://api.slack.com/reference/surfaces/formatting#mentioning-groups *)
 let mention_usergroup group_id = sprintf "<!subteam^%s>" group_id
+let mention_re = Re2.create_exn {|\B@[a-zA-Z0-9][a-zA-Z0-9._-]*|}
+
+let highlight_mentions get_id_by_slack_name s =
+  let subst s =
+    String.lchop s
+    |> get_id_by_slack_name
+    |> Option.map (function
+      | `User u -> mention_user u
+      | `Group g -> mention_usergroup g
+      )
+    |> Option.default s
+  in
+  Re2.replace_exn mention_re s ~f:(fun m -> subst (Re2.Match.get_exn ~sub:(`Index 0) m))
