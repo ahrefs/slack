a53c224178d6d07c1303d53d81ac4c1ec96dcb79
koonwen <koonwen@gmail.com>
2025-03-08 16:55:08 +0800

    upload_file: support snippets

--- lib/api_remote.ml
+++ lib/api_remote.ml
@@ -113,27 +113,33 @@ let complete_upload_external ~(ctx : Context.t) ~(req : Slack_t.complete_upload_
     ~name:(sprintf "files.completeUploadExternal (%s)" @@ Slack_j.string_of_files_v2 req.files)
     ~body `POST "files.completeUploadExternal" Slack_j.read_complete_upload_ext_res
 
-(** [upload_file ctx file] upload [file] to channels noted in [file.channels]
-      with content [file.content]; Not supporting file upload through form using
-      `file` currently
-      uses web API with access token *)
+(** [upload_file ctx file] upload [file] to channels noted in
+    [file.channels] with content [file.content]; Not supporting file
+    upload through form using `file` currently uses web API with
+    access token. Snippet can also be sent instead by filling the
+    content value *)
 let upload_file ~(ctx : Context.t) ~(req : Slack_t.files_upload_req) =
-  match req.filename, req.content with
-  | None, _ | _, None -> Exn.fail "need to supply both filename and content"
-  | Some filename, Some content ->
-    let length = String.length content in
+  match req.filename with
+  | None -> Exn.fail "need to supply filename"
+  | Some filename ->
+    let contents, length =
+      match req.content with
+      | Some v -> v, String.length v
+      | None ->
+        let raw_file_contents = In_channel.with_open_bin filename (fun ic -> input_all ic) in
+        raw_file_contents, String.length raw_file_contents
+    in
     let req' = Slack_j.make_get_upload_url_ext_req ~filename ~length () in
     ( match%lwt get_upload_url_external ~ctx ~req:req' with
     | Error e -> Lwt.return_error e
     | Ok { upload_url; file_id; _ } ->
-      let raw_file_contents = In_channel.with_open_bin filename (fun ic -> input_all ic) in
-      let body = `Raw ("text/plain", raw_file_contents) in
+      let body = `Raw ("text/plain", contents) in
       ( match%lwt http_request ~ua:ctx.ua ~body `POST upload_url with
       | Error e -> slack_lib_fail "upload file failed with: %s" e
       | Ok _ ->
         let files : Slack_t.files_v2 = [ { id = file_id; title = req.title } ] in
-        let req = Slack_j.make_complete_upload_ext_req ~files ?channels:req.channels ?thread_ts:req.thread_ts () in
-        ( match%lwt complete_upload_external ~ctx ~req with
+        let req' = Slack_j.make_complete_upload_ext_req ~files ?channels:req.channels ?thread_ts:req.thread_ts () in
+        ( match%lwt complete_upload_external ~ctx ~req:req' with
         | Error e -> Lwt.return_error e
         | Ok { files; _ } ->
           let f =
